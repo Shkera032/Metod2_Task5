c01b0d0623c0e1ef8f19ea917ea299257a516bc0
Shkera032 <forsakengod032@gmail.com>
2023-05-15 12:52:34 +0300

    через Array.sort?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,29 +9,16 @@ package org.example;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+
 public class Main {
     public static void main(String[] args) {
         int[] arr= { 6, 5, 4, 3, 2, 50, 7, 8, 9, 10, 11, 12 };
-        int max =0;
-        int i = 0;
-        int min = arr[i];
-        for (i = 0; i< arr.length; i++){
+        Arrays.sort(arr);
+       // for (int num : arr) {
+            //System.out.println(num + " ");
+       // }
 
-            System.out.println(" ________________ " + "Итерация - " + i + " ________________ ");
-            System.out.println("Значение arr[" + i + "] = " + arr[i]);
-            System.out.println("Значение max: " + max);
-            System.out.println("Значение min: " + min);
-            if (max<arr[i]){
-                max=arr[i];
-
-            } else if(min>arr[i]){
-                min=arr[i];
-
-            }
-        }
-        System.out.println("___________________________________Конец цикла_________________________");
-        System.out.println("Значение max: " + max);
-        System.out.println("Значение min: " + min);
+        System.out.println(arr[arr.length-1]);
 
     }
 }
